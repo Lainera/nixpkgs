b2748e053b3ff77b7be96c6f6f9e872ce7ee9146
Izorkin <izorkin@elven.pw>
2023-11-20 23:08:06 +0300

    angie-console-light: init at 1.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3271,6 +3271,8 @@ with pkgs;
     openssl = quictls;
   };
 
+  angie-console-light = callPackage ../servers/http/angie/console-light.nix { };
+
   angle-grinder = callPackage ../tools/text/angle-grinder { };
 
   ansifilter = callPackage ../tools/text/ansifilter { };
